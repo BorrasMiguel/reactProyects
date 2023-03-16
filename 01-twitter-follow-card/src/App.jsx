06c8c890e11c8f11ce429cd13c749095a65496a3
BorrasMiguel <miguelborras94@gmail.com>
2023-03-16 13:17:01 +0100

    React proyects

--- 01-twitter-follow-card/src/App.jsx
+++ 01-twitter-follow-card/src/App.jsx
@@ -3,19 +3,14 @@ import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 const users = [
   {
-    userName: 'midudev',
-    name: 'Miguel Ángel Durán',
+    userName: 'IbaiLlanos',
+    name: 'Ibai',
     isFollowing: true
   },
   {
     userName: 'pheralb',
     name: 'Pablo H.',
     isFollowing: false
-  },
-  {
-    userName: 'PacoHdezs',
-    name: 'Paco Hdez',
-    isFollowing: true
   },
   {
     userName: 'TMChein',
